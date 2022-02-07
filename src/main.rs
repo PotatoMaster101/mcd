7ba7d3afcdb027cf78c3a8ebc52cf983aa1a409f
PotatoMaster101 <PotatoMaster101@users.noreply.github.com>
2022-02-07 18:57:44 +0800

    Add sneak key option

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,10 @@ const MIN_INTERVAL: u32 = 75;
 #[clap(about = "Automatically press CTRL and left click in random intervals, use CAPS-LOCK to toggle")]
 struct Cli {
     #[clap(short, long, display_order = 1, help = "Milliseconds for maximum wait time, defaults to 75")]
-    pub interval: Option<u32>
+    pub interval: Option<u32>,
+
+    #[clap(short, long, display_order = 2, help = "Sneak key to press, defaults to CTRL")]
+    pub key: Option<String>
 }
 
 fn main() {
@@ -24,15 +27,19 @@ fn main() {
         None => MIN_INTERVAL + 1,
         Some(i) => if i <= MIN_INTERVAL { MIN_INTERVAL + 1 } else { i }
     };
+    let key = match args.key {
+        None => LControlKey,
+        Some(i) => if i == "SHIFT" { LShiftKey } else { LControlKey }
+    };
 
     CapsLockKey.bind(move || {
         let mut rng = rand::thread_rng();
         while CapsLockKey.is_toggled() {
             sleep(Duration::from_millis(rng.gen_range(MIN_INTERVAL..interval).into()));
-            LControlKey.press();
+            key.press();
             LeftButton.press();
             sleep(Duration::from_millis(MIN_INTERVAL.into()));
-            LControlKey.release();
+            key.release();
             LeftButton.release();
         }
     });
